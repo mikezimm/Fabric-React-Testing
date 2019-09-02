70a1cc4fabdf99c6fa8adfd7bc63b1cedcb98337
Karina Stan <karina.stan@autoliv.com>
2019-09-02 10:21:46 +0300

    test

---
+++ src/webparts/aPeoplePickerReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAPeoplePickerReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'APeoplePickerReactWebPartStrings' {
+  const strings: IAPeoplePickerReactWebPartStrings;
+  export = strings;
+}

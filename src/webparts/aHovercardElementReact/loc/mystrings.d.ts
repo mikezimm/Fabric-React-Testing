bf7f4ccc8488b4aec978450e706eafe6f26f0e96
MikeZimm <mike.zimmerman@autoliv.com>
2019-09-02 12:58:29 -0400

    Added React Hover Card with simple element - example 1

---
+++ src/webparts/aHovercardElementReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAHovercardElementReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AHovercardElementReactWebPartStrings' {
+  const strings: IAHovercardElementReactWebPartStrings;
+  export = strings;
+}

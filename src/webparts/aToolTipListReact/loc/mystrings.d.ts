b529db9dd40adac1bf304d634aea93313f3e4af2
MikeZimm <mike.zimmerman@autoliv.com>
2019-09-02 13:12:12 -0400

    Added basic Tool Tip with List

---
+++ src/webparts/aToolTipListReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAToolTipListReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AToolTipListReactWebPartStrings' {
+  const strings: IAToolTipListReactWebPartStrings;
+  export = strings;
+}

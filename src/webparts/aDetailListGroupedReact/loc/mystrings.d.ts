00ecde437bded27c6069668c231b739232097c9c
MikeZimm <mike.zimmerman@autoliv.com>
2019-09-03 20:28:10 -0400

    Added Detail List Grouped

---
+++ src/webparts/aDetailListGroupedReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IADetailListGroupedReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ADetailListGroupedReactWebPartStrings' {
+  const strings: IADetailListGroupedReactWebPartStrings;
+  export = strings;
+}

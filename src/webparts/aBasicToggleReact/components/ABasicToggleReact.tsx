4a5616d15cad2e7cb5dc50f74b515dbd21407754
MikeZimm <mike.zimmerman@autoliv.com>
2019-09-04 00:04:04 -0400

    Added Stateless and Function Components - Dropdwns, Toggles and Icons (in the A Bunch of..."

--- src/webparts/aBasicToggleReact/components/ABasicToggleReact.tsx
+++ src/webparts/aBasicToggleReact/components/ABasicToggleReact.tsx
@@ -3,6 +3,42 @@ import styles from './ABasicToggleReact.module.scss';
 import { IABasicToggleReactProps } from './IABasicToggleReactProps';
 import { escape } from '@microsoft/sp-lodash-subset';
 
+//Start adding this to basic React Generated part.
+
+import { Toggle } from 'office-ui-fabric-react/lib/Toggle';
+import { Stack, IStackTokens } from 'office-ui-fabric-react/lib/Stack';
+
+const stackTokens: IStackTokens = { childrenGap: 10 };
+
+export const ToggleBasicExample: React.FunctionComponent = () => {
+  return (
+    <Stack tokens={stackTokens}>
+      <Toggle label="Enabled and checked" defaultChecked onText="On" offText="Off" onChange={_onChange} />
+
+      <Toggle label="Enabled and unchecked" onText="On" offText="Off" onChange={_onChange} />
+
+      <Toggle label="Disabled and checked" defaultChecked disabled onText="On" offText="Off" />
+
+      <Toggle label="Disabled and unchecked" disabled onText="On" offText="Off" />
+
+      <Toggle label="With inline label" inlineLabel onText="On" offText="Off" onChange={_onChange} />
+
+      <Toggle label="Disabled with inline label" inlineLabel disabled onText="On" offText="Off" />
+
+      <Toggle label="With inline label and without onText and offText" inlineLabel onChange={_onChange} />
+
+      <Toggle label="Disabled with inline label and without onText and offText" inlineLabel disabled />
+    </Stack>
+  );
+};
+
+function _onChange(ev: React.MouseEvent<HTMLElement>, checked: boolean) {
+  console.log('toggle is ' + (checked ? 'checked' : 'not checked'));
+}
+
+//After this, insert into dom.
+
+
 export default class ABasicToggleReact extends React.Component<IABasicToggleReactProps, {}> {
   public render(): React.ReactElement<IABasicToggleReactProps> {
     return (
@@ -10,12 +46,16 @@ export default class ABasicToggleReact extends React.Component<IABasicToggleReac
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Problem with TOGGLEs example!</span>
-              <p className={ styles.subTitle }>Not able to create Toggle yet because it's a FucntionComponent, not a class :(.</p>
+
+              <span className={ styles.title }>TOGGLEs example!</span>
+              <p className={ styles.subTitle }>Sample is for a constant that can be inserted into JSX like below</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://developer.microsoft.com/en-us/fabric#/controls/web/toggle" target="_blank" className={ styles.button }>
                 <span className={ styles.label }>Fabric UI Docs</span>
               </a>
+              <br/><br/>
+              <p><ToggleBasicExample /></p>
+
             </div>
           </div>
         </div>
---
+++ src/webparts/aBunchOfFunctionComponentsReact/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IABunchOfFunctionComponentsReactWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ABunchOfFunctionComponentsReactWebPartStrings' {
+  const strings: IABunchOfFunctionComponentsReactWebPartStrings;
+  export = strings;
+}
